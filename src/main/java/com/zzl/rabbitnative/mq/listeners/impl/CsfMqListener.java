1048c8e029e19720eec84dc5fcd4de472daee820
zhangzhaolin <zhangzhaolin@inspur.com>
2019-11-05 11:18:28 +0800

    mq

--- src/main/java/com/zzl/rabbitnative/mq/listeners/impl/CsfMqListener.java
+++ src/main/java/com/zzl/rabbitnative/mq/listeners/impl/CsfMqListener.java
@@ -8,34 +8,33 @@ import com.zzl.rabbitnative.mq.endpoints.impl.CsfEndpoint;
 import com.zzl.rabbitnative.mq.listeners.MqListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * Created by zhangzhaolin on 2019/11/4.
  */
 @Component
+@ConfigurationProperties(prefix = "rabbitmq.listen")
 public class CsfMqListener implements MqListener {
 
     private CsfEndpoint endpoint;
 
-    @Value("${rabbitmq.listen.csf.exchange}")
-    private String exchangeName;
-
-    @Value("${rabbitmq.listen.csf.queue}")
-    private String queueName;
-
-    @Value("${rabbitmq.listen.csf.routing-key}")
-    private String routingKey;
+    private Map<String, Map<String, String>> csf;
 
     @Autowired
     public CsfMqListener(CsfEndpoint endpoint) {
         this.endpoint = endpoint;
     }
 
-    @Override
-    public void listen() {
+    public void listenToA() {
+        Map<String, String> map = csf.get("funcA");
+        String exchangeName = map.get("exchange");
+        String queueName = map.get("queue");
+        String routingKey = map.get("routing-key");
         try {
             Channel channel = endpoint.getChannel();
             channel.exchangeDeclare(exchangeName, "topic", true);
@@ -64,5 +63,50 @@ public class CsfMqListener implements MqListener {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void listenToB() {
+        Map<String, String> map = csf.get("funcB");
+        String exchangeName = map.get("exchange");
+        String queueName = map.get("queue");
+        String routingKey = map.get("routing-key");
+        try {
+            Channel channel = endpoint.getChannel();
+            channel.exchangeDeclare(exchangeName, "topic", true);
+            channel.queueDeclare(queueName, true, false, false, null);
+            channel.queueBind(queueName, exchangeName, routingKey);
+            boolean autoAck=false;
+            //消息消费完成确认
+            channel.basicConsume(queueName, autoAck,"", new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(String consumerTag,
+                                           Envelope envelope,
+                                           AMQP.BasicProperties properties,
+                                           byte[] body) throws IOException {
+                    try {
+                        String message = new String(body);
+                        System.out.println("消费者 csf == " + message);
+                    }
+                    catch (Exception e) {
+                        channel.abort();  //此操作中的所有异常将被丢弃
+                    }
+                    finally {
+                        channel.basicAck(envelope.getDeliveryTag(),false);
+                    }
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void listen() {
+        listenToA();
+        listenToB();
+    }
+
+    public void setCsf(Map<String, Map<String, String>> csf) {
+        this.csf = csf;
     }
 }
